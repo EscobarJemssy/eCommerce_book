4b0ab87f18827c30e06424e15b9c404abca87f9d
EscobarJemssy <jemssyjarelly@gmail.com>
2025-05-23 21:18:32 -0500

    creating the carousel

--- src/components/Carrusel.jsx
+++ src/components/Carrusel.jsx
@@ -1,18 +1,23 @@
 import React, { useState, useEffect } from "react";
 import ContCarrusel from "./subComponents/ContCarrusel";
+import jsonData from "../assets/google_books_1299_clean_1.json"; // revisar la ruta
 
 const Carrusel = () => {
-
-
+    const media_rating = 4.274 
+    const median_voters = [1382, 38526] //135.0
+    const best_sold = jsonData.filter(book => book.rating && book.rating >= media_rating).slice(0, 5);
+    const rec_book = jsonData.filter(rec => rec.voters && rec.voters >= median_voters[0] && rec.voters <= median_voters[1]).sort((a, b) => b.voters - a.voters)
+    const random_book = [...jsonData].sort(() => Math.random() - 0.5).slice(0, 5);
     return (
         <>
 
             <div class="carrusel">
-
-                <ContCarrusel categoria="" />
-                <ContCarrusel categoria="" />
-                <ContCarrusel categoria="" />
-                
+                <h2>Libros más vendidos</h2>
+                <ContCarrusel category={best_sold} />
+                <h2>Libros recomendados</h2>
+                <ContCarrusel category={rec_book} />
+                <h2>Libros de temporada</h2>
+                <ContCarrusel category={random_book} />
             </div>
 
         </>
--- src/components/subComponents/ContCarrusel.jsx
+++ src/components/subComponents/ContCarrusel.jsx
@@ -1,13 +1,18 @@
 import React, { useState, useEffect } from "react";
 
-const ContCarrusel = (props) => {
-
+const ContCarrusel = ({category}) => {
     return (
-        <div>
-            <p>Holo *2 {props.categoria}</p>
+        <div className="carrusel-horizontal">
+            {category.map((cat) => (
+                <div key={cat.index} className="cat">
+                    <img src=""/>
+                    <h3>{cat.title}</h3>
+                    <p>{cat.author}</p>
+                    <p>{cat.price}</p>
+                </div>
+            ))}
         </div>
     );
-
 }
 
 export default ContCarrusel;
